1c8bdb8f6d6baa51743dd8d64385bcf4d7983d0d
NatalieShik <144128581+NatalieShik@users.noreply.github.com>
2023-09-05 16:05:55 +0300

    Add files via upload

---
+++ FirstVersion/InformationForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace FirstVersion
+{
+    partial class InformationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InformationForm));
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.textBoxInfo = new System.Windows.Forms.TextBox();
+            this.pictureBoxForIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonOK.Location = new System.Drawing.Point(444, 331);
+            this.buttonOK.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(83, 27);
+            this.buttonOK.TabIndex = 1;
+            this.buttonOK.Text = "Ok";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // textBoxInfo
+            // 
+            this.textBoxInfo.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxInfo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxInfo.Enabled = false;
+            this.textBoxInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInfo.Location = new System.Drawing.Point(324, 39);
+            this.textBoxInfo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBoxInfo.Multiline = true;
+            this.textBoxInfo.Name = "textBoxInfo";
+            this.textBoxInfo.Size = new System.Drawing.Size(301, 279);
+            this.textBoxInfo.TabIndex = 2;
+            this.textBoxInfo.Text = resources.GetString("textBoxInfo.Text");
+            // 
+            // pictureBoxForIcon
+            // 
+            this.pictureBoxForIcon.Image = global::FirstVersion.Properties.Resources.table_icon_png_2__1_;
+            this.pictureBoxForIcon.Location = new System.Drawing.Point(25, 48);
+            this.pictureBoxForIcon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBoxForIcon.Name = "pictureBoxForIcon";
+            this.pictureBoxForIcon.Size = new System.Drawing.Size(293, 271);
+            this.pictureBoxForIcon.TabIndex = 0;
+            this.pictureBoxForIcon.TabStop = false;
+            // 
+            // InformationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 368);
+            this.Controls.Add(this.textBoxInfo);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.pictureBoxForIcon);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(654, 415);
+            this.MinimizeBox = false;
+            this.Name = "InformationForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Справка";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxForIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxForIcon;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.TextBox textBoxInfo;
+    }
+}
---
+++ FirstVersion/MainForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FirstVersion
+{
+    public partial class MainForm : Form
+    {
+        List<FrequencyAnalysis> frequencyList = new List<FrequencyAnalysis>();  //лист, предназначенный для хранения символов и информации о них
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InformationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InformationForm form = new InformationForm();   //при нажатии на кнопку справки создается новый объект формы справки и демонстрируется пользователю
+            form.ShowDialog();
+        }
+
+        private void buttonCount_Click(object sender, EventArgs e)
+        {
+            frequencyList.Clear();  //стирание записей прошлого анализа
+            Dictionary<char, int> frequency = new Dictionary<char, int>();  //словарь для подсчета ключей и их значений
+            int symbolAmount = 0;   //переменная для подсчета общего числа анализируемых символов
+            foreach (char symbol in richTextBoxForUserInput.Text)   //цикл для посимвольного анализа данных в поле ввода
+            {
+                char tempSymbol = symbol;   //записывается текущий символ
+                int tempNum = (int)tempSymbol;  //и его номер в Юникоде
+                if (Char.IsControl(tempSymbol)) //если символ относится к управляющим, цикл пропускает его
+                    continue;
+
+                if (tempNum >= 8234 && tempNum <= 8238 //если это символы переворота
+                    || tempNum == 8206 || tempNum == 8207 
+                    || tempNum == 173)  //или мягкий дефис (т.е. символ, указывающий, где можно переносить слово)
+                    continue;   //цикл пропускает и эти символы
+
+                if (Char.IsWhiteSpace(tempSymbol) || tempNum == 8203)   //если текущий символ пробел (в том числе пробел нулевой ширины)
+                    tempSymbol = ' ';   //то указываем, что это просто пробел
+
+                if ((int)tempSymbol == 8209)    //если символ - это неразрывный дефис, записываем его как обычный дефис
+                    tempSymbol = '-';
+
+                if (!frequency.ContainsKey(tempSymbol)) //если в словаре еще нет текущего символа, добавляем его с ключом (абсолютной встречаемостью) равной 1
+                    frequency.Add(tempSymbol, 1);
+                else
+                    frequency[tempSymbol]++;    //иначе увеличиваем значение у уже существующего в словаре символа на 1
+                symbolAmount++; //увеличиваем количество проанализированных символов на 1
+            }
+
+            if (frequency.Count == 0)   //если в словаре нет записей, выводим ошибку, очищаем данные из таблицы, данные о количестве символов и завершаем анализ
+            {
+                MessageBox.Show("Нельзя проанализировать отсутствующий текст.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                dataGridViewForResults.RowCount = 0;
+                labelForNumber.Text = "не рассчитано";
+                return;
+            }
+
+            foreach (var item in frequency.OrderByDescending(x => x.Value)) //в упорядоченном по убыванию ключа (абсолютной частоты) словаре перебирается каждый элемент
+            {
+                string symbol;  //переменная для хранения строкового значения/определения символа
+                string toolTip = $"Десятичный код в Юникоде: {(int)item.Key}";  //переменная, записывающая информацию о символе для подсказки
+                if (item.Key == ' ')    //если встретился пробел, то вместо пустого графического отображения, будет слово, подсказка будет из этого же слова, так как пробелы могут быть разными, как и коды
+                {
+                    symbol = "Пробел";
+                    toolTip = symbol;
+                }
+                else
+                    symbol = item.Key.ToString();   //в ином случае символ будет преобразован к типу string
+                if (item.Key == '-')    //пробел может быть как обычным, так и неразрывным, поэтому подсказка не отображает код
+                    toolTip = "Дефис";
+                FrequencyAnalysis temp = new FrequencyAnalysis(symbol, item.Value, Math.Round(((double)item.Value) / symbolAmount, 4) * 100, toolTip); //создание нового объекта с информацией о символе
+                frequencyList.Add(temp);    //добавление объекта в лист
+            }
+
+            int i = 0;  //переменная для перебора строк в таблице
+            dataGridViewForResults.RowCount = frequencyList.Count;  //указываем, что в таблице столько же колонок, сколько записей в листе
+            foreach (FrequencyAnalysis symbolInfo in frequencyList) //для каждой записи в листе
+            {
+                dataGridViewForResults.Rows[i].Cells[0].Value = symbolInfo.Symbol;  //записываем в первую колонку сам символ
+                dataGridViewForResults.Rows[i].Cells[0].ToolTipText = symbolInfo.ToolTipText;   //устанавливаем для этой колонки подсказку при наведении
+                dataGridViewForResults.Rows[i].Cells[1].Value = symbolInfo.Absolutefrequency;   //записываем абсюлутную встречаемость во вторую колонку
+                dataGridViewForResults.Rows[i].Cells[2].Value = symbolInfo.Relativefrequency + "%"; //и относительную в третьей
+                i++;
+            }
+            labelForNumber.Text = symbolAmount.ToString();  //записываем над таблицей общее количество символов
+        }
+    }
+}
