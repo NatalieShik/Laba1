a8e2a9084cc24a5f6ffcf2c941a163e3c92082fa
NatalieShik <144128581+NatalieShik@users.noreply.github.com>
2024-06-20 18:02:48 +0300

    Test

---
+++ UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibraryDEMOPrepearing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectDEMOPrepearing
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void ПолучитьКоличествоЗаявок_ВозвращаетПравильноеКоличество()
+        {
+            // Arrange
+            int expected = 2;
+
+            // Act
+            int result = Statistic.ПолучитьКоличествоЗаявок(1);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
